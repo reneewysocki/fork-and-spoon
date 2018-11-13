d17fbb7c47f39e4fc92196b082f9e2d1204fa90b
Renee Wysocki <reneew311@gmail.com>
2018-11-13 16:55:00 -0600

    pretty much the whole thing

---
+++ app/data/friends.js
@@ -0,0 +1,104 @@
+let friends = [
+    {
+        "name": "Michael",
+        "photo": "https://media1.popsugar-assets.com/files/thumbor/eOF2Umn-mqNGnohxrtjeurwWDmI/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2018/08/20/677/n/1922283/1118a12c5b7adb1e342de9.55515725_/i/Michael-Scott-Misquotations-Office-Video.jpg",
+        "scores": [
+            3,
+            4,
+            1,
+            1,
+            5,
+        ]
+    },
+    {
+        "name": "Jim",
+        "photo": "https://nypdecider.files.wordpress.com/2017/11/jim-halpert.jpg",
+        "scores": [
+            2,
+            3,
+            3,
+            1,
+            1,
+        ]
+    },
+    {
+        "name": "Pam",
+        "photo": "https://theofficeanalytics.files.wordpress.com/2017/09/pam-halpert-1.jpg",
+        "scores": [
+            4,
+            2,
+            1,
+            5,
+            4,
+        ]
+    },
+    {
+        "name": "Dwight",
+        "photo": "https://techinsight.net/wp-content/uploads/2017/08/635939099971870877-2008862771_dwightC-768x432.jpg",
+        "scores": [
+            2,
+            5,
+            1,
+            3,
+            4,
+        ]
+    },
+    {
+        "name": "Creed",
+        "photo": "https://media.altpress.com/uploads/2018/07/creed_the_office.jpg",
+        "scores": [
+            5,
+            3,
+            3,
+            4,
+            2,
+        ]
+    },
+    {
+        "name": "Meredith",
+        "photo": "https://cdn.playbuzz.com/cdn//3fb269ba-436d-45f3-86b6-6b3ffaa5acec/20e87244-a113-4d5f-aec2-90c610038439.jpg",
+        "scores": [
+            5,
+            3,
+            1,
+            4,
+            3,
+        ]
+    },
+    {
+        "name": "Kevin",
+        "photo": "https://pbs.twimg.com/profile_images/515307069533331457/J-THo7yG_400x400.jpeg",
+        "scores": [
+            2,
+            2,
+            1,
+            5,
+            2,
+        ]
+    },
+    {
+        "name": "Angela",
+        "photo": "https://i.kinja-img.com/gawker-media/image/upload/s--Dp1E1m_f--/c_scale,f_auto,fl_progressive,q_80,w_800/b7xni3yk1feftxc1d0zk.jpg",
+        "scores": [
+            1,
+            5,
+            1,
+            2,
+            4,
+        ]
+    },
+    {
+        "name": "Andy",
+        "photo": "http://images.fanpop.com/images/image_uploads/Andy--andy-bernard-181921_400_225.jpg",
+        "scores": [
+            3,
+            4,
+            1,
+            1,
+            1,
+        ]
+    },
+    
+];
+
+module.exports = friends;
